2a51f835926c4161748f487e59348ffe52afebaf
id-tsubasa <23w2012b@shinshu-u.ac.jp>
2023-08-22 13:18:47 +0900

    9

--- main.go
+++ main.go
@@ -4,15 +4,35 @@ import (
 	"fmt"
 	"net/http"
 )
+
+//jsonFileWriteという配列を作成
+var jsonFileWrite = `[{"lat":35.658034,"lng":139.701636,"name":"東京駅"},{"lat":35.681167,"lng":139.767052,"name":"新宿駅"},{"lat":35.710063,"lng":139.8107,"name":"池袋駅"},{"lat":35.729189,"lng":139.710002,"name":"上野駅"},{"lat":35.658034,"lng":139.701636,"name":"東京駅"}]`
+
+
 func main() {
-    http.HandleFunc("/hello", HelloServer)
-	http.HandleFunc("/good", HelloServer2)
+	http.HandleFunc("/gps", handler)
 	fmt.Println("start server")
-    http.ListenAndServe("localhost:8080", nil)
+    http.ListenAndServe("localhost:3000", nil)
 }
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s", r.URL.Query().Get("lat"))
-}
-func HelloServer2(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "good, %s", r.URL.Path[:])
+
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    // レスポンスヘッダの設定
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+
+    // ステータスコードの設定
+    // この後でレスポンスヘッダの設定はできない
+    
+
+    // レスポンスボディの書き込み
+    w.Write([]byte(jsonFileWrite))
+    // http.Requestはio.Writerとして扱えるため、これでもOK
+    // fmt.Fprintln(w, "Bad request!")
+	latitude := r.FormValue("lat")
+	lontitude := r.FormValue("lon")
+	if latitude!=""&&lontitude!=""{
+		//goglemapのAPIを叩く
+	}else{
+		fmt.Println("No data")
+	}
 }
--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+//import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 void main() {
   // 最初に表示するWidget
@@ -25,34 +27,34 @@ class MyTodoApp extends StatelessWidget {
 }
 
 // リスト一覧画面用Widget
-class TodoListPage extends StatelessWidget {
-  const TodoListPage({super.key});
+// class TodoListPage extends StatelessWidget {
+//   const TodoListPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Center(
-        child: Text('GPS情報の取得'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // "push"で新規画面に遷移
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) {
-              // 遷移先の画面としてリスト追加画面を指定
-              return const MyGPS();
-            }),
-          );
-        },
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: const Center(
+//         child: Text('GPS情報の取得'),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           // "push"で新規画面に遷移
+//           Navigator.of(context).push(
+//             MaterialPageRoute(builder: (context) {
+//               // 遷移先の画面としてリスト追加画面を指定
+//               return const MyGPS();
+//             }),
+//           );
+//         },
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
 
 //GPS情報を取得する
-class MyGPS extends StatelessWidget {
-  const MyGPS({super.key});
+class TodoListPage extends StatelessWidget {
+  const TodoListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -64,28 +66,77 @@ class MyGPS extends StatelessWidget {
         child: TextButton(
           child: const Text('近くのお店を検索'),
           onPressed: () async {
-            //現在のGPS情報を取得する
-            final position = await Geolocator.getCurrentPosition(
-                desiredAccuracy: LocationAccuracy.high);
-            //positionから緯度経度を取得して表示する
-            final latitude = position.latitude;
-            final longitude = position.longitude;
-            // ignore: use_build_context_synchronously
-            await showDialog<void>(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: const Text('現在地'),
-                  content: Text('緯度：$latitude\n経度：$longitude'),
-                  actions: <Widget>[
-                    TextButton(
-                      child: const Text('OK'),
-                      onPressed: () => Navigator.pop(context),
-                    ),
-                  ],
-                );
-              },
+            //新しい画面にresponse.bodyを表示する;
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) {
+                return const FutureBuilderPage();
+              }),
             );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class FutureBuilderPage extends StatefulWidget {
+  const FutureBuilderPage({super.key});
+
+  @override
+  State<FutureBuilderPage> createState() => serchPage();
+}
+
+class serchPage extends State<FutureBuilderPage> {
+  late Future<String> future;
+
+  //localhost:3000のサーバーにhttp通信で現在地を送信する関数
+  _getMYgps() async {
+    //現在地を取得する
+    final position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    //現在地の緯度を取得する
+    final latitude = position.latitude.toString();
+    //現在地の経度を取得する
+    final longitude = position.longitude.toString();
+
+    //laitudeとlongitudeをreturnする
+    return [latitude, longitude];
+  }
+
+  Future<String> http_post() async {
+    //200--success
+    var gps = await _getMYgps();
+    var response = await http.post(
+        Uri.parse("http://localhost:3000/gps?lat=${gps[0]}&lon=${gps[1]}"));
+    //「緯度：latitude,経度：longitude」をprintする
+    print("緯度：${gps[0]}、経度：${gps[1]}}");
+    return (response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    future = Future(() async {
+      return await http_post();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('GPS情報'),
+      ),
+      body: Center(
+        child: FutureBuilder(
+          future: future,
+          builder: (context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data!);
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            return const CircularProgressIndicator();
           },
         ),
       ),
