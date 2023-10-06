cb77b02b2284710614e5ebb0be10db6904fb3aeb
iWings0501 <103626431+i-wings0501@users.noreply.github.com>
2023-10-06 13:32:19 +0900

    Additional features of Google Maps (#5)
    * add:google map api key setting
    
    * add:additional packages needed on the front side
    
    * add:added Google Maps functionality
    
    * add:obtaining routing information

--- back_go/lib/call_googlemaps_api/call_googlemaps_api.go
+++ back_go/lib/call_googlemaps_api/call_googlemaps_api.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"labs/lib/env"
 	"log"
+	"strconv"
 
 	"googlemaps.github.io/maps"
 )
@@ -30,10 +31,29 @@ func GetPlaceInfo(latitude float64, longitude float64, purpose string) maps.Plac
 		// Type :"PlaceType",
 		// PageToken: "string",
 	}
-	route, err := c.NearbySearch(context.Background(), r)
+	place_info, err := c.NearbySearch(context.Background(), r)
 	if err != nil {
 		log.Fatalf("fatal error: %s", err)
 	}
 
-	return route
+	return place_info
+}
+
+func GetPlaceRoute(latitude float64, longitude float64, place_id string) []maps.Route {
+	key := env.ReadEnv()
+	c, err := maps.NewClient(maps.WithAPIKey(key))
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+	r := &maps.DirectionsRequest{
+		Origin:      strconv.FormatFloat(latitude,'f',-1,64) + "," + strconv.FormatFloat(longitude,'f',-1,64),
+		Destination: "place_id:" + place_id,
+		Language: "ja",
+	}
+	place_route, _, err := c.Directions(context.Background(), r)
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+
+	return place_route
 }
--- back_go/main.go
+++ back_go/main.go
@@ -8,20 +8,59 @@ import (
 	"strconv"
 )
 
-
-type Place struct {
+// 店舗情報を格納する構造体
+type PlaceInfo struct {
+	// 店舗名
 	Name string `json:"name"`
+
+	// 店舗ID
 	PlaceID string `json:"place_id"`
 }
 
+// 経路情報を格納する構造体
+type PlaceRoute struct {
+	// 出発地点と目的地
+	Legs struct{
+		StartLocation struct{
+			Lat float64 `json:"lat"`
+			Lng float64 `json:"lng"`
+		} `json:"start_location"`
+		EndLocation struct{
+			Lat float64 `json:"lat"`
+			Lng float64 `json:"lng"`
+		} `json:"end_location"`
+	} `json:"legs"`
+
+	// 方向性(道順)
+	OverviewPolyline struct{
+		Points string `json:"points"`
+	} `json:"overview_polyline"`
+
+	// 表示境界枠
+	Bounds struct {
+		Northeast struct {
+			Lat float64 `json:"lat"`
+			Lng float64 `json:"lng"`
+		} `json:"northeast"`
+		Southwest struct {
+			Lat float64 `json:"lat"`
+			Lng float64 `json:"lng"`
+		} `json:"southwest"`
+	} `json:"bounds"`
+}
+
 func main() {
-	http.HandleFunc("/gps", handler)
+	// 店舗情報の取得
+	http.HandleFunc("/gps", gps_handler)
+	// 経路情報の取得
+	http.HandleFunc("/route", route_handler)
+
 	fmt.Println("start server")
     http.ListenAndServe("localhost:3000", nil)
 }
 
-
-func handler(w http.ResponseWriter, r *http.Request) {
+// 店舗情報の取得
+func gps_handler(w http.ResponseWriter, r *http.Request) {
     // レスポンスヘッダの設定
     w.Header().Set("Access-Control-Allow-Origin", "*")
 
@@ -31,6 +70,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	longitude := r.FormValue("lon")
 	purpose := r.FormValue("purpose")
 
+	// 文字列をfloat64に変換(緯度経度)
 	lat64, err := strconv.ParseFloat(latitude, 64)
 	if err != nil {
         fmt.Printf("%s\n", err.Error())
@@ -44,8 +84,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// 各値が空でないか確認
 	if latitude!=""&&longitude!=""&&purpose!=""{
-		//googlemapsのAPIを叩く
+		// googlemapsのAPIを叩く準備完了
 		fmt.Printf("latitude: %s, longitude: %s, purpose: %s\n", latitude, longitude, purpose)
 	}
 	if purpose==""{
@@ -55,22 +96,134 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
     // レスポンスボディの書き込み
-	data := call_googlemaps_api.GetPlaceInfo(lat64,lng64,purpose)
-	PlaceList := []Place{}
-	// PlaceID := []string{}
-	//リスト名ResultNameとして、data.Results[i].Nameの要素をfor文で取得
-	for i := 0; i < len(data.Results); i++ {
-		PlaceList = append(PlaceList,Place{data.Results[i].Name, data.Results[i].PlaceID});
+	PlaceInfoData := call_googlemaps_api.GetPlaceInfo(lat64,lng64,purpose)
+	PlaceInfoList := []PlaceInfo{}
+	//// PlaceID := []string{}
+	// リストPlaceInfoListに店舗名、店舗IDをfor文で格納
+	for i := 0; i < len(PlaceInfoData.Results); i++ {
+		PlaceInfoList = append(PlaceInfoList,PlaceInfo{PlaceInfoData.Results[i].Name, PlaceInfoData.Results[i].PlaceID});
 	}
 
 	// json形式に変換
-	jsonBytes, err := json.Marshal(PlaceList)
+	InfoJsonBytes, err := json.Marshal(PlaceInfoList)
 	if err != nil {
 		fmt.Println("JSON Marshal error:", err)
 		return
 	}
 
-    w.Write(jsonBytes)
-    
+    w.Write(InfoJsonBytes)
 }
 
+//経路情報の取得
+func route_handler(w http.ResponseWriter, r *http.Request) {
+    // レスポンスヘッダの設定
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+
+    // ステータスコードの設定
+    // この後でレスポンスヘッダの設定はできない
+	latitude := r.FormValue("lat")
+	longitude := r.FormValue("lon")
+	place_id := r.FormValue("place_id")
+
+	// 文字列をfloat64に変換(緯度経度)
+	lat64, err := strconv.ParseFloat(latitude, 64)
+	if err != nil {
+        fmt.Printf("%s\n", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+	lng64, err := strconv.ParseFloat(longitude, 64)
+	if err != nil {
+		fmt.Printf("%s\n", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// 各値が空でないか確認
+	if latitude!=""&&longitude!=""&&place_id!=""{
+		// googlemapsのAPIを叩く準備完了
+		fmt.Printf("latitude: %s, longitude: %s, place_id: %s\n", latitude, longitude, place_id)
+	}
+	if place_id==""{
+		fmt.Printf("place_idが空です\n")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+    // レスポンスボディの書き込み
+	PlaceRouteData := call_googlemaps_api.GetPlaceRoute(lat64,lng64,place_id)
+	PlaceRouteList := []PlaceRoute{}
+	
+	// リストPlaceRouteListに出発地点と目的地、方向性(道順)、表示境界枠を格納
+	for _, v := range PlaceRouteData {
+		PlaceRouteList = append(PlaceRouteList, PlaceRoute{
+			Legs: struct{
+				StartLocation struct{
+					Lat float64 `json:"lat"`
+					Lng float64 `json:"lng"`
+				} `json:"start_location"`
+				EndLocation struct{
+					Lat float64 `json:"lat"`
+					Lng float64 `json:"lng"`
+				} `json:"end_location"`
+			}{
+				StartLocation: struct{
+					Lat float64 `json:"lat"`
+					Lng float64 `json:"lng"`
+				}{
+					Lat: v.Legs[0].StartLocation.Lat,
+					Lng: v.Legs[0].StartLocation.Lng,
+				},
+				EndLocation: struct{
+					Lat float64 `json:"lat"`
+					Lng float64 `json:"lng"`
+				}{
+					Lat: v.Legs[0].EndLocation.Lat,
+					Lng: v.Legs[0].EndLocation.Lng,
+				},
+			},
+
+			OverviewPolyline: struct{
+				Points string `json:"points"`
+			}{
+				Points: v.OverviewPolyline.Points,
+			},
+
+			Bounds: struct {
+				Northeast struct {
+					Lat float64 `json:"lat"`
+					Lng float64 `json:"lng"`
+				} `json:"northeast"`
+				Southwest struct {
+					Lat float64 `json:"lat"`
+					Lng float64 `json:"lng"`
+				} `json:"southwest"`
+			}{
+				Northeast: struct {
+					Lat float64 `json:"lat"`
+					Lng float64 `json:"lng"`
+				}{
+					Lat: v.Bounds.NorthEast.Lat,
+					Lng: v.Bounds.NorthEast.Lng,
+				},
+				Southwest: struct {
+					Lat float64 `json:"lat"`
+					Lng float64 `json:"lng"`
+				}{
+					Lat: v.Bounds.SouthWest.Lat,
+					Lng: v.Bounds.SouthWest.Lng,
+				},
+			},
+		})
+	}
+	
+	// json形式に変換
+	RouteJsonBytes, err := json.Marshal(PlaceRouteList)
+	if err != nil {
+		fmt.Println("JSON Marshal error:", err)
+		return
+	}
+
+	// pretty.Println(PlaceRouteList[0].Legs.StartLocation.Lat)
+    w.Write(RouteJsonBytes)
+}
