649400a59faedfbbf15599156bc8f8917ae88706
id-tsubasa <103626431+i-wings0501@users.noreply.github.com>
2023-10-12 13:22:30 +0900

    add:get photo reference function

--- back_go/lib/get_photo_data/get_photo_data.go
+++ back_go/lib/get_photo_data/get_photo_data.go
@@ -1,1 +1,25 @@
 package get_photo_data
+
+import (
+	"fmt"
+	"labs/lib/call_googlemaps_api"
+)
+
+type PlaceDetail struct {
+	// Name string `json:"name"`
+	Photos []struct{
+		PhotoReference string `json:"photo_reference"`
+	} `json:"photos, omitempty"`
+}
+
+func ReturnPhotoReference(place_id string) []PlaceDetail {
+	PlaceDetailData := call_googlemaps_api.GetPlaceDetail(place_id)
+	PlaceDetailList := []PlaceDetail{}
+	for _, place := range PlaceDetailData.Photos {
+		PlaceDetailList = append(PlaceDetailList, PlaceDetail{Photos: []struct{
+			PhotoReference string `json:"photo_reference"`
+		}{{place.PhotoReference}}})
+	}
+	return PlaceDetailList
+}
+
